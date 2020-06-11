237a88c543e298d4b9235b5f57458b409b983e22
XCASHAnimus <lovro@x-network.io>
2020-06-11 18:16:38 +0300

    [Jail system] update

--- cogs/jailHandlerCogs.py
+++ cogs/jailHandlerCogs.py
@@ -7,9 +7,9 @@ sys.path.append(project_path)
 from datetime import datetime
 from datetime import timedelta
 import time
-from backoffice.spamSystemDb import SpamSystemManager
 import discord
 from discord import Member as DiscordMember
+from backoffice.jailSystemDb import JailSystemManager
 from discord.ext import commands
 from discord.ext.commands import Greedy
 from jailList import JailManagement
@@ -18,16 +18,16 @@ from cogs.toolsCog.systemMessages import CustomMessages
 from colorama import Fore
 
 helper = Helpers()
-community_manager = SpamSystemManager()
+jail_sys_manager = JailSystemManager()
 jail_manager = JailManagement()
-customMessages = CustomMessages()
+custom_message = CustomMessages()
 bot_setup = helper.read_json_file(file_name='mainBotConfig.json')
 
 def is_community_registered(ctx):
-    return community_manager.check_community_reg_status(community_id=ctx.message.guild.id)
+    return jail_sys_manager.check_if_jail_registered(community_id=ctx.message.guild.id)
 
 def is_community_not_registered(ctx):
-    return community_manager.check_if_not_registered(community_id=ctx.message.guild.id)
+    return jail_sys_manager.check_if_jail_not_registered(community_id=ctx.message.guild.id)
 
 def is_public(ctx):
     return ctx.message.channel.type != discord.ChannelType.private
@@ -55,17 +55,46 @@ class JailService(commands.Cog):
                       'value': "Auto language moderation category of settings for community with auto jail and release function"},
                      ]
 
-            await customMessages.embed_builder(ctx=ctx, title=title, description=description, data=value)
+            await custom_message.embed_builder(ctx=ctx, title=title, description=description, data=value)
             
             
-    # Turn on and turn off
-    @jail.command()
-    async def on (self,ctx):
-        pass
+    @spam.command()
+    async def on(self,ctx):
+        try:
+            await ctx.message.delete()
+        except Exception:
+            pass
+        if jail_sys_manager.check_welcome_channel_status(community_id=int(ctx.message.guild.id)):
+            if jail_sys_manager.check_reaction_message_status(community_id=int(ctx.message.guild.id)):
+                if jail_sys_manager.turn_on_off(community_id=int(ctx.message.guild.id),direction=1):
+                    title='__System Message__'
+                    message = 'You have turned ON the bot invasion prevention function successfully. '
+                    await custom_message.system_message(ctx=ctx, color_code=0, message = message, destination = 1, sys_msg_title=title)
+                else:
+                    message = f'There was a backend error. Please try again later or contact one of the administrators on the community. We apologize for inconvinience'
+                    await custom_message.system_message(ctx, message=message, color_code=1, destination=1)
+            else:
+                message = f'You can not turn this service ON since the message where system will be listening for reacions, has not been provided yet. Please use first command {bot_setup["command"]}spam set_message <message id as INT> ***' 
+                await custom_message.system_message(ctx, message=message, color_code=1, destination=1)
+        else:
+            message = f'You can not turn this service ON since the channel where system will be listening for reacions, has not been provided yet. Please use first command {bot_setup["command"]}spam set_channel <#discord.TextChannel> ***' 
+            await custom_message.system_message(ctx, message=message, color_code=1, destination=1)
     
-    @jail.command()
-    async def off (self,ctx):
-        pass
+    
+    @spam.command()
+    async def off(self,ctx):
+        try:
+            await ctx.message.delete()
+        except Exception:
+            pass
+        if spam_sys_mng.turn_on_off(community_id=int(ctx.message.guild.id),direction=0):
+                title='__System Message__'
+                message = 'You have turned OFF the bot invasion prevention function successfully. Have in mind that now everything will needd to be done manually.'
+                await custom_message.system_message(ctx=ctx, color_code=0, message = message, destination = 1, sys_msg_title=title)
+        else:
+            message = f'There was a backend error. Please try again later or contact one of the administrators on the community. We apologize for inconvinience'
+            await custom_message.system_message(ctx, message=message, color_code=1, destination=1)
+
     
             
 def setup(bot):
