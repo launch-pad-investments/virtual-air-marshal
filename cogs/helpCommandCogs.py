f721675517e08b4b3511f4895d4a587177fa0fca
XCASHAnimus <lovro@x-network.io>
2020-06-15 13:10:43 +0300

    update

--- cogs/helpCommandCogs.py
+++ cogs/helpCommandCogs.py
@@ -1,20 +1,22 @@
+"""
+COGS: used for help commands
+"""
+
 import os
 import sys
 
 import discord
 from discord.ext import commands
-from discord import Permissions, Colour
-from git import Repo, InvalidGitRepositoryError
-from cogs.menuTest import HelpGeneralMenu
+from discord import Permissions, Embed
+from cogs.toolsCog.books import GeneralHelp
 
 from utils.jsonReader import Helpers
 from cogs.toolsCog.systemMessages import CustomMessages
-from cogs.toolsCog.checks import is_overwatch
 
 project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_path)
 customMessages = CustomMessages()
-menu = HelpGeneralMenu()
+menu = GeneralHelp()
 helper = Helpers()
 
 bot_setup = helper.read_json_file(file_name='mainBotConfig.json')
@@ -25,8 +27,18 @@ class HelpCommands(commands.Cog):
         self.bot = bot    
     
     @commands.command()
-    async def help(self,ctx):
-        await menu.start(ctx)
+    async def help(self, ctx):
+        if ctx.invoked_subcommand is None:
+            pass
+            
+        
+    @help.command()
+    async def jail(self, ctx):
+        pass
+    
+    @help.command()
+    async def spam(self, ctx):
+        pass
         
 def setup(bot):
     bot.add_cog(HelpCommands(bot))
